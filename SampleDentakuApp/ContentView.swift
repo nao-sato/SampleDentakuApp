40ee67e7c808b57067e8b2099dae29c1aa9c02ff
nao-sato <72682955+nao-sato@users.noreply.github.com>
2021-02-11 14:15:00 +0900

    Initial Commit

---
+++ SampleDentakuApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleDentakuApp
+//
+//  Created by 佐藤尚 on 2021/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleDentakuApp/SampleDentakuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDentakuAppApp.swift
+//  SampleDentakuApp
+//
+//  Created by 佐藤尚 on 2021/02/11.
+//
+
+import SwiftUI
+
+@main
+struct SampleDentakuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
